d2339f45b3c23053730230c1d7d6c017ef32280c
angelo <angelonambs@gmail.com>
2025-11-27 16:46:27 +0300

    refactor: Improve the structure of the message and user schemas.

--- src/models/message.js
+++ src/models/message.js
@@ -1,29 +1,70 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate-v2");
 
-const messageSchema = mongoose.Schema({
-  text : {
-    type: String,
-    trim: true,
-    required: true
+const messageSchema = mongoose.Schema(
+  {
+    text: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    show: {
+      type: Boolean,
+      default: false,
+    },
+    from: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "user",
+    },
+    to: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "user",
+    },
   },
-  vue : {
-    type: Boolean,
-    default: false
-  },
-  from : {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'User'
-  },
-  to : {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'User'
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true
-})
+);
 
-const Message = mongoose.model('message', messageSchema)
+// Filtrer les reponses
+messageSchema.methods.toJSON = function () {
+  const message = this;
 
-module.exports = Message
+  const messageObject = message.toObject();
+  messageObject.id = messageObject._id;
+
+  delete messageObject._id;
+  delete messageObject.__v;
+  if (this.populated("to")) {
+    const {
+      _id: id,
+      name,
+      email,
+      avatar,
+      createdAt,
+      updatedAt,
+    } = messageObject.to;
+    messageObject.to = { id, name, email, avatar, createdAt, updatedAt };
+  }
+  if (this.populated("from")) {
+    const {
+      _id: id,
+      name,
+      email,
+      avatar,
+      createdAt,
+      updatedAt,
+    } = messageObject.from;
+    messageObject.from = { id, name, email, avatar, createdAt, updatedAt };
+  }
+
+  return messageObject;
+};
+
+messageSchema.plugin(mongoosePaginate);
+
+const Message = mongoose.model("message", messageSchema);
+
+module.exports = Message;
--- src/models/user.js
+++ src/models/user.js
@@ -1,96 +1,101 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const userSchema = mongoose.Schema({
-  name : {
-    type: String,
-    required: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
-    lowercase: true
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 7,
-    validate(value){
-      if(value.toLowerCase().includes('password')){
-        throw new Error('Password cannot contain "password"')
-      }
+const ERROR_MESSAGES = {
+    EMAIL_PASSWORD_REQUIRED: "EMAIL_PASSWORD_REQUIRED",
+    USER_NOT_FOUND: "USER_NOT_FOUND",
+    UNABLE_PASSWORD: "UNABLE_PASSWORD",
+};
+
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+            lowercase: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        bio: {
+            type: String,
+            required: false,
+            trim: true,
+            maxlength: 150,
+        },
+        phone: {
+            type: String,
+            required: false,
+            trim: true,
+            length: 10,
+        },
+        refreshToken: [String],
+        avatar: {
+            type: String,
+        },
+        socketId: {
+            type: String,
+            required: false,
+        },
+    },
+    {
+        timestamps: true,
     }
-  },
-  tokens: [{
-    token: {
-      type: String,
-      required: true
-    }
-  }],
-  avatar: {
-    type: String
-  }
-}, {
-  timestamps: true
-})
+);
 
-// Filtrer les reponses 
-userSchema.methods.toJSON = function () { 
-  const user = this
-  const userObject = user.toObject()
-  
-  delete userObject.password
-  delete userObject.tokens
+// Filtrer les reponses
+userSchema.methods.toJSON = function () {
+    const user = this;
 
-  return userObject
-}
+    const userObject = user.toObject();
+    userObject.id = userObject._id;
 
-// Generer un token
-userSchema.methods.generateAuthToken = async function() {
-  const user = this
-  const token = jwt.sign({ 
-    _id: user._id,
-    exp: Math.floor(Date.now() / 1000) + 3600 * 48
-   }, process.env.TOKEN_SECRET_KEY)
-  
-  user.tokens = user.tokens.concat({token})
-  await user.save()
+    delete userObject._id;
+    delete userObject.__v;
+    delete userObject.password;
+    delete userObject.refreshToken;
+    delete userObject.socketId;
 
-  return token
-}
+    return userObject;
+};
 
 // Verifier le mot de passe
 userSchema.statics.findByCredentials = async function (email, password) {
-  const user = await User.findOne({email})
-  
-  if (!user) {
-    throw new Error ('Unable email')
-  }
+    if (!email || !password) throw { status: 400, message: ERROR_MESSAGES.EMAIL_PASSWORD_REQUIRED };
+    const user = await User.findOne({ email });
 
-  const isMatch = await bcrypt.compare(password, user.password)
-  
-  if (!isMatch) {
-    throw new Error ('Unable password')
-  }
-  return user
-}
+    if (!user) {
+        throw { status: 401, message: ERROR_MESSAGES.USER_NOT_FOUND };
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        throw { status: 401, message: ERROR_MESSAGES.UNABLE_PASSWORD };
+    }
+    return user;
+};
 
 // Hasher le mot de passe
-userSchema.pre('save', async function (next) {
-  const user = this
+userSchema.pre("save", async function (next) {
+    const user = this;
 
-  if(user.isModified('password')){
-    user.password = await bcrypt.hash(user.password, 8)
-  }
+    if (user.isModified("password")) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
 
-  next()
-})
+    next();
+});
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("user", userSchema);
 
-module.exports = User
+module.exports = User;
